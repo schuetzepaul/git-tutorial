ea4ce1f9edab5e8bed9aa275fce2a7e4fca7e960
Paul Schuetze <paul.schuetze@desy.de>
2017-08-14 14:51:51 +0200

    Initial commit, working stage

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    char name[100];
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << endl;
+    
+    cout << "Hello " << name << "!" << endl;
+
+}
