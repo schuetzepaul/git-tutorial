40850440b73ef1982c717e1a1863d9e6e507b997
Paul Schuetze <paul.schuetze@desy.de>
2017-08-14 15:26:36 +0200

    Added function sayGoodbye Function to say goodbye to the user

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 void sayHello(const char*);
+void sayGoodbye(const char*);
 
 int main(){
 
@@ -13,9 +14,14 @@ int main(){
     cout << endl;
     
     sayHello(name);
+    sayGoodbye(name);
 }
 
 // New function
 void sayHello(const char* name){
     cout << "Hello " << name << "!" << endl;
+}
+
+void sayGoodbye(const char* name){
+    cout << "Goodbye " << name << "!" << endl;
 }
